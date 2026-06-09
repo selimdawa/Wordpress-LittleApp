451cafe677c2b76ffd329106bcff98469e5dfb86
selimdawa <selimdawa@gmail.com>
2026-06-09 03:57:25 +0300

    SDK 37 / Android 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,15 +7,13 @@ plugins {
 android {
     namespace = "com.littleapp.wordpress"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+        version = release(37)
     }
 
     defaultConfig {
         applicationId = "com.littleapp.wordpress"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
